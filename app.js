933da7d3562c1dd53d9a428f662091fdd6c92964
Ahmad Moqanasa <ahmad.moqanasa@gmail.com>
2016-01-23 21:18:15 +0400

    entry point

---
+++ app.js
@@ -0,0 +1,10 @@
+var express = require('express'),
+    bodyParser = require('body-parser'),
+    app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.listen(3000, function () {
+    console.log('app started at localhost:3000')
+});
