4205f4f7cdd9d20a77b7917db225b68094363fee
Ahmad Moqanasa <ahmad.moqanasa@gmail.com>
2016-01-23 21:22:12 +0400

    api route

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var express = require('express'),
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+
+app.use('/api/', require('./handlers/api'));
 app.listen(3000, function () {
     console.log('app started at localhost:3000')
 });
