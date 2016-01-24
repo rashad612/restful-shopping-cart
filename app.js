da44dcfeb210b07a3a9f2fc4f85f0ea4b0a4a0df
Ahmad Moqanasa <ahmad.moqanasa@gmail.com>
2016-01-24 06:14:55 +0400

    move db connections to config

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 var express = require('express'),
     bodyParser = require('body-parser'),
     app = express(),
-    mongoose = require('mongoose');
+    mongoose = require('mongoose'),
+    dbConfig = require('./config/db');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-mongoose.connect('mongodb://localhost/cart')
+mongoose.connect(dbConfig.CONNECTION_STRING)
 
 app.use('/api/', require('./handlers/api'));
 app.listen(3000, function () {
---
+++ config/db.js
@@ -0,0 +1,4 @@
+module.exports = {
+    CONNECTION_STRING: 'mongodb://localhost/cart',
+    TEST_CONNECTION_STRING: 'mongodb://localhost/cart-test'
+};
