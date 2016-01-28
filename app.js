70a0ef5260a34d78f457a347a0b1d59dc077e280
Ahmad Moqanasa <ahmo@one.com>
2016-01-28 15:55:07 +0400

    fix config path and add port options

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express'),
     bodyParser = require('body-parser'),
     app = express(),
-    dbConfig = require('../config/db'),
+    dbConfig = require('./config/db'),
     db = require('./modules/db');
 
 db.connect(dbConfig);
@@ -9,6 +9,8 @@ db.connect(dbConfig);
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use('/api/', require('./handlers/api'));
-app.listen(3000, function () {
-    console.log('app started at localhost:3000')
+app.set('port', process.env.PORT);
+
+app.listen(function () {
+    console.log('app started at localhost:' + app.get('port'));
 });
